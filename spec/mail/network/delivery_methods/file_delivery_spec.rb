807b8d20b70da1244118111959f57aff320559cb
neilang <neilang@gmail.com>
2013-07-30 21:07:44 +1000

    Append .log extension when writing files with FileDelivery method

--- spec/mail/network/delivery_methods/file_delivery_spec.rb
+++ spec/mail/network/delivery_methods/file_delivery_spec.rb
@@ -15,14 +15,14 @@ describe "SMTP Delivery Method" do
                                :enable_starttls_auto => true  }
     end
   end
-  
+
   after(:each) do
     files = Dir.glob(File.join(Mail.delivery_method.settings[:location], '*'))
     files.each do |file|
       File.delete(file)
     end
   end
-  
+
   describe "general usage" do
     tmpdir = File.expand_path('../../../../tmp/mail', __FILE__)
 
@@ -30,15 +30,15 @@ describe "SMTP Delivery Method" do
       Mail.defaults do
         delivery_method :file, :location => tmpdir
       end
-      
+
       mail = Mail.deliver do
         from    'roger@moore.com'
         to      'marcel@amont.com'
         subject 'invalid RFC2822'
       end
-      
-      delivery = File.join(Mail.delivery_method.settings[:location], 'marcel@amont.com')
-      
+
+      delivery = File.join(Mail.delivery_method.settings[:location], 'marcel@amont.com.log')
+
       File.read(delivery).should eq mail.encoded
     end
 
@@ -46,16 +46,16 @@ describe "SMTP Delivery Method" do
       Mail.defaults do
         delivery_method :file, :location => tmpdir
       end
-      
+
       mail = Mail.deliver do
         from    'roger@moore.com'
         to      'marcel@amont.com, bob@me.com'
         subject 'invalid RFC2822'
       end
-      
-      delivery_one = File.join(Mail.delivery_method.settings[:location], 'marcel@amont.com')
-      delivery_two = File.join(Mail.delivery_method.settings[:location], 'bob@me.com')
-      
+
+      delivery_one = File.join(Mail.delivery_method.settings[:location], 'marcel@amont.com.log')
+      delivery_two = File.join(Mail.delivery_method.settings[:location], 'bob@me.com.log')
+
       File.read(delivery_one).should eq mail.encoded
       File.read(delivery_two).should eq mail.encoded
     end
@@ -64,13 +64,13 @@ describe "SMTP Delivery Method" do
       Mail.defaults do
         delivery_method :file, :location => tmpdir
       end
-      
+
       Mail.deliver do
         from    'roger@moore.com'
         to      '"Long, stupid email address" <mikel@test.lindsaar.net>'
         subject 'invalid RFC2822'
       end
-      delivery = File.join(Mail.delivery_method.settings[:location], 'mikel@test.lindsaar.net')
+      delivery = File.join(Mail.delivery_method.settings[:location], 'mikel@test.lindsaar.net.log')
       File.exists?(delivery).should be_true
     end
 
@@ -78,14 +78,14 @@ describe "SMTP Delivery Method" do
       Mail.defaults do
         delivery_method :file, :location => tmpdir
       end
-      
+
       Mail.deliver do
         from    'roger@moore.com'
         to      '../../../../../../../../../../../tmp/pwn'
         subject 'evil hacker'
       end
 
-      delivery = File.join(Mail.delivery_method.settings[:location], 'pwn')
+      delivery = File.join(Mail.delivery_method.settings[:location], 'pwn.log')
       File.exists?(delivery).should be_true
     end
 
@@ -118,5 +118,5 @@ describe "SMTP Delivery Method" do
     end
 
   end
-  
+
 end
